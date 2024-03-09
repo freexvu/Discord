5ef6c4cdae06f910ea95c8aa6943d7bb7e222f71
freexvu <82249864+freexvu@users.noreply.github.com>
2024-03-08 20:03:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
 const Discord = require('discord.js-selfbot-v13');
 const client = new Discord.Client({
   readyStatus: false,
-  checkUpdate: false
+  checkUpdate: true
 });
 
-const keepAlive = require('./server.js');
-keepAlive();
-
-function formatTime() { //Credits to himika#0001 and never#0001
-  const date = new Date();
-  const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
-    hour12: true,
-    hour: 'numeric',
-    minute: 'numeric'
-  };
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
+const keepAlive = require('./server.js')
+keepAlive()
 
 client.on('ready', async () => {
-  console.clear();
-  console.log(`${client.user.tag} - rich presence started!`);
 
-  const r = new Discord.RichPresence()
-    .setApplicationId('')
-    .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
-    .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
-  client.user.setActivity(r);
-  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+    console.clear();
 
-  let prevTime = null;
-  setInterval(() => {
-    const newTime = formatTime();
-    if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
-      r.setDetails(newDetails);
-      client.user.setActivity(r);
-      prevTime = newTime;
-    }
-  }, 1000); // Update every second
-});
+    console.log(`${client.user.tag} - rich presence started!`
+               )
+const r = new Discord.RichPresence()
+    .setApplicationId('857147640851202068')
+    .setType('STREAMING') //STREAMING, PLAYING, LISTENING.
+    .setURL('https://twitch.tv/xdd') // or your yt url
+    .setName('##1600')
+    .setDetails('$')
+     client.user.setActivity(r);
+     client.user.setPresence({ status: "online" }); //dnd, online, idle, offline
+})
 
-const mySecret = process.env['TOKEN'];
-client.login(mySecret);
+client.login(process.env.TOKEN)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Copyright to mrnekrozyt 2021-2023
+// Support Server:https://discord.gg/pSJ5JkmH6N
+// Source Code: https://github.com/mrnekrozyt/Streaming-24-7-RPC/
